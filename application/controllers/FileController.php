b043ef4f63c46bf7119e88f8af9b7842f27bf8f8
jmacj <jm.nz0723@gmail.com>
2018-02-12 10:39:07 +0800

    updated initial models

--- application/controllers/FileController.php
+++ application/controllers/FileController.php
@@ -12,15 +12,45 @@ class FileController extends BaseController
 
 	public function project($id) {
 
-		foreach ($this->thread->with('reply')->get_many_by(["project_id" => $id]) as $thread) {
-			
-			unset($thread['project_id']);
-			unset($thread['deleted']);
-			$data['threads'][]=$thread;
-		}
 		$project = $this->project->get($id);
 		$this->session->set_userdata(['project' => $project]);
 
+		// creates new root folder of project if there is none
+		$folder = $this->folder->get($id);
+		if(!isset($folder)){
+			$data = [
+				'id' => $id,
+				'name' => $project['name'],
+				'root' => TRUE
+			];
+			$this->folder->insert($data);
+		}
+
 		return parent::main_page("file/index", $data ?? null);
 	}
+
+	/* ************** These are functions to be used when creating new folders.. to be updated ***************** @author JM
+	public function create_folder($id) {
+
+		$data = [
+			'id' => //generate id,
+			'name'=> $this->input->post('name'),
+			'location' => $id,
+			'root' => FALSE
+
+		];
+
+		return $this->folder->insert($data);
+	}
+
+	public function update_folder($id) {
+
+		$data = [
+			'name' => $this->input->post('name'),
+			'location' => $id
+		];
+
+		return $this->folder->update($data)
+	}
+	*/
 }
